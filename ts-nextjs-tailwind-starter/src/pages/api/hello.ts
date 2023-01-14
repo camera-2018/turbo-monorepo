5c85bded2e38475345aa9f9636bda3508ba92b6b
camera-2018 <2907618001@qq.com>
2023-01-14 15:54:05 +0800

    chore: prettier format

--- ts-nextjs-tailwind-starter/src/pages/api/hello.ts
+++ ts-nextjs-tailwind-starter/src/pages/api/hello.ts
@@ -3,5 +3,6 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
 export default function hello(req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json({ name: 'Bambang' });
+  const a = 1;
+  res.status(200).json({ name: a });
 }
